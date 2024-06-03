1e4b8cf5c92d464b0ed263b47f087fb4ab6054b6
Sadaru-Hansaka <sandaruhansaka0828@gmail.com>
2024-06-03 18:29:34 +0530

    Created a base code to control data of the db, from this code, can add items and their prices to the data base and delete all from the data base and search item and price from item name

--- handle-db.py
+++ handle-db.py
@@ -5,7 +5,7 @@ import sqlite3
 connection = sqlite3.connect("database.db")
 cursor = connection.cursor()
 
-
+cursor.execute("DROP TABLE IF EXISTS Items")
 cursor.execute('''
         CREATE TABLE IF NOT EXISTS Items (
                Item_name TEXT NOT NULL,
@@ -14,6 +14,11 @@ cursor.execute('''
 ''')
 
 connection.commit()
+
+cursor.execute("PRAGMA table_info(Items)")
+columns = cursor.fetchall()
+for column in columns:
+    print(column)
 
 while True:
     choice = input("""
@@ -27,3 +32,33 @@ while True:
         cursor.execute("INSERT INTO Items (Item_name, Price) VALUES (?, ?)", (item, price))
         connection.commit()
         print("Data inserted successfully!")
+
+    elif choice == "2":
+        search = input("Search by item name: ")
+        cursor.execute("SELECT * FROM Items WHERE Item_name LIKE? OR Item_name LIKE ?",
+                       ('%' + search + '%', '%' + search + '%'))
+        result = cursor.fetchall()
+        if result:
+            for row in result:
+                print(row)
+        else:
+            print("No matching items found.")
+
+    elif choice == "3":
+        print("Editing part")
+    
+    elif choice=="q":
+        cursor.execute('DELETE  FROM Items')
+        connection.commit()
+        print("Database Cleared")
+        break
+
+    else:
+        cursor.execute('SELECT * FROM Items')
+        out = cursor.fetchall()
+        for r in out:
+            print(f"{r[0]} - Rs.{r[1]}")
+        break
+
+cursor.close()
+connection.close()
